c8bf527740627a2daf832fe4b8dece598155b493
turbocep <162988238+turbocep@users.noreply.github.com>
2024-11-11 14:45:33 +0100

    create ::cipher

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,2 +1,10 @@
+# No-state class that ciphers and deciphers messages
 class CaesarCipher
+  def self.cipher(phrase, shift = 0)
+    return '' if phrase == ''
+
+    phrase.split('').map do |char|
+      (char.ord + shift).chr
+    end.join('')
+  end
 end
