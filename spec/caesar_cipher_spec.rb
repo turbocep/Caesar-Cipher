dc54b80d69e8ac26455f15e7a9fd9a839346b6ae
turbocep <162988238+turbocep@users.noreply.github.com>
2024-11-11 17:20:31 +0100

    test ::cipher and ::decipher together [ci pass]
    I tested if a phrase equals the result of it being
    ciphered and then deciphered.

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -62,5 +62,73 @@ describe CaesarCipher do
     end
   end
   describe '.decipher' do
+    context 'when empty string is given as phrase' do
+      it 'returns empty string' do
+        result = CaesarCipher.decipher('', 2)
+        expect(result).to eq('')
+      end
+    end
+    context 'when alphabet is given as phrase' do
+      context 'when shift is 0' do
+        it 'returns input phrase' do
+          phrase = 'abcdefghijklmnopqrstuvwxyz'
+          shift = 0
+          result = CaesarCipher.decipher(phrase, shift)
+          expect(result).to eq(phrase)
+        end
+      end
+      context 'when shift is 1' do
+        it 'returns input phrase' do
+          phrase = 'bcde'
+          shift = 1
+          result = CaesarCipher.decipher(phrase, shift)
+          expect(result).to eq('abcd')
+        end
+      end
+      context 'when shift is -1' do
+        it 'returns input phrase' do
+          phrase = 'abcd'
+          shift = -1
+          result = CaesarCipher.decipher(phrase, shift)
+          expect(result).to eq('bcde')
+        end
+      end
+      context 'when shift is 5' do
+        it 'returns input phrase' do
+          phrase = 'fghijkl'
+          shift = 5
+          result = CaesarCipher.decipher(phrase, shift)
+          expect(result).to eq('abcdefg')
+        end
+      end
+    end
+    context 'when shift is 2' do
+      let(:shift) { 2 }
+      context "and 'Hello World!' is given as phrase" do
+        it "returns 'Jgnnq Yqtnf!" do
+          phrase = 'Jgnnq Yqtnf!'
+          expect(CaesarCipher.decipher(phrase, shift)).to eq('Hello World!')
+        end
+      end
+      context "and 'I have 5 fingers on each hand' is given as a phrase" do
+        it "returns 'K jcxg 5 hkpigtu qp gcej jcpf'" do
+          phrase = 'K jcxg 5 hkpigtu qp gcej jcpf'
+          out_phrase = 'I have 5 fingers on each hand'
+          expect(CaesarCipher.decipher(phrase, shift)).to eq(out_phrase)
+        end
+      end
+    end
+  end
+  describe '.cipher && .decipher' do
+    context 'when phrase is ciphered, then deciphered' do
+      it 'equals the initial phrase' do
+        initial_phrase = 'Hi there! My name is Turbocep. My favorite symbol is
+                          the octothorpe(#) and my favorite number is 42!'
+        shift = 5
+        ciphered = CaesarCipher.cipher(initial_phrase, shift)
+        deciphered = CaesarCipher.decipher(ciphered, shift)
+        expect(deciphered).to eq(initial_phrase)
+      end
+    end
   end
 end
