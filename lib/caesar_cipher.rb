18cda49da9496680f046829ec8eb99586f53b27b
turbocep <162988238+turbocep@users.noreply.github.com>
2024-11-11 17:14:33 +0100

    stop using #ord and create ::cipher_helper
    The problem with using #ord is that I found it
    difficult to only include valid characters. Cases
    would return invalid chars, and then they couldn't
    be deciphered back. I instead made characters
    shift with an hard-coded array. This makes it easy
    to include characters that we'd like to shift.
    
    I noticed that ::cipher and ::decipher only had
    one different character. To adhere to DRY, I kept
    one copy as a helper method with an extra variable
    for the difference. I'm not sure how maintainable
    this is. I'll learn a better method eventually.

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,13 +1,31 @@
 # No-state class that ciphers and deciphers messages
 class CaesarCipher
-  VALID_CHARS = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z].freeze
+  VALID_CHARS = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x
+                   y z].freeze
+  def self.valid_chars_max_index
+    VALID_CHARS.length - 1
+  end
 
-  def self.cipher(phrase, shift)
+  def self.cipher_helper(phrase, shift, symbol)
     phrase.split('').map do |char|
-      VALID_CHARS.include?(char.downcase) ? (char.ord + shift).chr : char
+      next char unless VALID_CHARS.include?(char)
+
+      new_ord = VALID_CHARS.find_index(char) + (shift * symbol)
+      if new_ord > valid_chars_max_index
+        VALID_CHARS[new_ord - valid_chars_max_index - 1]
+      else
+        VALID_CHARS[new_ord]
+      end
     end.join('')
   end
 
+  def self.cipher(phrase, shift)
+    cipher_helper(phrase, shift, 1)
+  end
+
   def self.decipher(phrase, shift)
+    cipher_helper(phrase, shift, -1)
   end
 end
+
+# Why not rewrite this with a linked list? That sounds so much easier to implement.
